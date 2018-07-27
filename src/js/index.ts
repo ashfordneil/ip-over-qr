f06897f3cd7611d999684c4dade6fc6882765c4e
Neil Ashford <ashfordneil0@gmail.com>
2018-07-27 21:00:27 +1000

    calling rust from typescript

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,10 @@
 console.log("Hello, world!");
+
+const main = async () => {
+    console.log("starting to load wasm");
+    const { foo } = await import("../rust/src/lib.rs");
+    console.log("finished loading wasm");
+    console.log(foo());
+}
+
+main()
