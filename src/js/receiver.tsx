1c8261522506ca4290682fe69c2e31681c285eca
Simon Gordon <gordonsimon@hotmail.com>
2018-07-28 15:19:41 +1000

    Hopefully works...?

--- src/js/receiver.tsx
+++ src/js/receiver.tsx
@@ -1,30 +1,60 @@
 import * as React from 'react';
 import { QrScanner } from './scanner';
 import { QRCanvas } from './qrcanvas';
-import getChecksum from './getChecksum';
 import { Scanned } from 'instascan';
 
 interface ReceiverProps {
 }
 
-interface ScanEntry {
-    frame: number;
-    content: Scanned;
-    checksum: string;
-}
+// interface ScanEntry {
+//     frame: number;
+//     content: Scanned;
+// }
 
 interface ReceiverState {
-    scanHist: Scanned[],
-    checksum: string | null;
+    initiated: boolean;
+    numFrames: number | null;
+    frames: { [id: number]: string },
+    requestedFrame: number | null;
 }
 
 export class Receiver extends React.Component<ReceiverProps, ReceiverState> {
     constructor(props: ReceiverProps) {
         super(props);
         this.state = {
-            scanHist: [],
-            checksum: null,
+            initiated: false,
+            numFrames: null,
+            frames: [],
+            requestedFrame: null,
         };
+    }
+
+    initiate(numFrames: number) {
+        this.setState({ numFrames, initiated: true });
+        this.requestNext();
+    }
+
+    requestNext(): boolean {
+        const next = this.nextFrame();
+        if (next) {
+            this.setState({ requestedFrame: next });
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    nextFrame(): number | null {
+        const { numFrames, frames } = this.state;
+        if (numFrames == null) {
+            throw new Error("Need num frames");
+        }
+        for (let i = 0; i < numFrames; i++) {
+            if (!(frames[i])) {
+                return i;
+            }
+        }
+        return null;
     }
 
     logScanned(scanned: Scanned) {
@@ -33,32 +63,45 @@ export class Receiver extends React.Component<ReceiverProps, ReceiverState> {
         if (match) {
             const content = match[1];
             const frame = parseInt(match[2]);
-            console.log(`frame ${frame}`);
+            const { requestedFrame } = this.state;
+            if (requestedFrame == null) {
+                throw new Error("requested frame is null, dunno how this happened");
+            }
+            if (frame == requestedFrame) {
+                this.setState(({ frames }) => {
+                    frames[frame] = content;
+                    return ({ frames });
+                });
+                const isDone = this.requestNext();
+                if (isDone) {
+                    console.log("done!");
+                    console.log(this.state.frames);
+                }
+            }
         }
         else {
             console.warn("Does not match format!");
         }
-        // const { scanHist, checksum } = this.state;
-        // const newChecksum = getChecksum(scanned);
-        // if (checksum != newChecksum) {
-        //     scanHist.push(scanned);
-        //     this.setState({ scanHist, checksum: newChecksum });
-        // }
     }
 
     render() {
         return <>
             {
-                this.state.checksum !== null
-                    ? <QRCanvas id="checksum" input={this.state.checksum} />
+                this.state.requestedFrame !== null
+                    ? <QRCanvas id="nextFrame" input={this.state.requestedFrame.toString()} />
                     : null
             }
             <QrScanner
                 autoscan
-                // display
-                // showAutoToggle
                 onScan={(scanned) => {
-                    if (scanned != null) {
+                    if (scanned == null) {
+                        return;
+                    }
+                    if (!this.state.initiated) {
+                        const { numFrames } = JSON.parse(scanned.content);
+                        this.initiate(numFrames);
+                    }
+                    else {
                         console.log("scanned!");
                         console.log(scanned);
                         this.logScanned(scanned.content);
